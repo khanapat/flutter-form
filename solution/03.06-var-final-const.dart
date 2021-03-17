5c9a6a5ca23c0b13efcb37d298f14eefa95343b9
Surasuk Oakkharaamonphong <surasuk612@gmail.com>
2021-03-17 13:05:32 +0700

    update

--- solution/03.06-var-final-const.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  // which variables should be const, final, var?
-  const text = 'I like pizza';
-  const topping = 'with tomatoes';
-  const favourite = '$text $topping';
-  String newText = favourite.replaceAll('pizza', 'pasta');
-  const newFavourite = 'Now I like curry';
-  print(newText);
-}
--- solution/06.03-sum-items-list.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var values = [1, 3, 5, 7, 9];
-  var sum = 0;
-  for (var value in values) {
-    sum += value;
-  }
-  print(sum);
-}
